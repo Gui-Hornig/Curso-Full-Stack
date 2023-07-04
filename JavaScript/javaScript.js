e5c4f1d12c96026863b3a9933732166a75a083fb
Gui-hornig <guihornig@gmail.com>
2023-07-04 10:14:40 -0300

    Update javaScript.js

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -17,8 +17,11 @@ console.log("teste")
     console.log("Os numeros pares sao", "2,4,6,8,10");
 }
 
+
+{   
 console.log("caneta");
 console.log(10); //valor literal 
 console.log(6.4);
 console.log(1.5);
-console.log(7.9);
+console.log(7.9); 
+}
