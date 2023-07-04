47f2d114d85ca4d50ee8074a1fb86e261719bbea
Gui-hornig <guihornig@gmail.com>
2023-07-04 10:09:12 -0300

    Update javaScript.js

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -1,5 +1,5 @@
-console.log(1+3);
-console.log("testando funcionalidades", 6+3);
+console.log(1 + 3);
+console.log("testando funcionalidades", 6 + 3);
 
 // console.log("testanto coentarios"); comentando no js
 
@@ -10,9 +10,9 @@ linhas
 */
 
 console.log("teste")
-{  
-    console.log("Os numeros impares sao > ","1,3,5,7,9");
+{
+    console.log("Os numeros impares sao > ", "1,3,5,7,9");
 }
 {
-    console.log("Os numeros pares sao","2,4,6,8,10")
+    console.log("Os numeros pares sao", "2,4,6,8,10");
 }
