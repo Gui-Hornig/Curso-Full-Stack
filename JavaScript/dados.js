061185c1f3ae76d4451e02b0c336bbf3ab491353
Gui-hornig <guihornig@gmail.com>
2023-07-04 10:41:24 -0300

    Update dados.js

--- JavaScript/dados.js
+++ JavaScript/dados.js
@@ -12,6 +12,21 @@
     console.log(quantidade); 
     console.log(preco);
     console.log(imposto);
-    console.log(precoFinal);
+    console.log(precoFinal); }
 
+{//sempre escolher bons preços e nomes claros as pastas e variáveis
+    let preco = 19.90;
+    let desconto = 0.4;
+
+    console.log(19.9 * 0.6);
+    console.log(preco * (1 - desconto)); //codigo mais limpo e claro
+
+    let precoComDesconto = preco * (1 - desconto);
+    console.log(precoComDesconto);
+
+
+
+    let nome = "caderno";
+    let categoria = "Papelaria"; 
+    console.log(nome + " " +  categoria); //Concatenando
 }
