a746ab1560c2e3d7a7905f4522792b2e18db44ea
Gui-hornig <guihornig@gmail.com>
2023-07-04 10:28:23 -0300

    Update javaScript.js

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -16,22 +16,3 @@ console.log("teste")
 {
     console.log("Os numeros pares sao", "2,4,6,8,10");
 }
-
-
-{   // console.log(7.9); valor literal 
-
-    let nome = ("Caneta");
-    const preco = 6.4;
-    const quantidade = 10;
-    const imposto = 1.5;
-    const precoFinal = preco + imposto
-
-    nome = "nome da coisa"
-
-    console.log(nome);
-    console.log(quantidade); 
-    console.log(preco);
-    console.log(imposto);
-    console.log(precoFinal);
-
-}
