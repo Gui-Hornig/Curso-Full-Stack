7f0f3a18089a1e8135dc3b4b656750a5ec7a3bc8
Gui-hornig <guihornig@gmail.com>
2023-07-04 10:28:06 -0300

    Create dados.js

---
+++ JavaScript/dados.js
@@ -0,0 +1,17 @@
+{   // console.log(7.9); valor literal 
+
+    let nome = ("Caneta");
+    const preco = 6.4;
+    const quantidade = 10;
+    const imposto = 1.5;
+    const precoFinal = preco + imposto
+
+    nome = "nome da coisa"
+
+    console.log(nome);
+    console.log(quantidade); 
+    console.log(preco);
+    console.log(imposto);
+    console.log(precoFinal);
+
+}
