59557e1308b7d44563d3ef82b9aaa2622f18d3bf
Gui-hornig <guihornig@gmail.com>
2023-07-06 17:10:44 -0300

    Update tampletString.js

--- JavaScript/Fundamento/tampletString.js
+++ JavaScript/Fundamento/tampletString.js
@@ -8,4 +8,5 @@ console.log(concatenacao, templete);
 // explessoes...
 console.log(` 1 + 1 = ${1+1}`);
 
-const up = texto => (`1+1 = ${1+1}`)
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up(`cuidado`)}!`)
