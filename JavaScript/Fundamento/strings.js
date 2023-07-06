2889c8ff342faf12a9b031ed3b0517143b9fba5e
Gui-hornig <guihornig@gmail.com>
2023-07-06 16:16:31 -0300

    Update strings.js

--- JavaScript/Fundamento/strings.js
+++ JavaScript/Fundamento/strings.js
@@ -6,4 +6,7 @@ console.log(escola.charCodeAt(3)); // retorna o código relacionado a tabela UNI
 console.log(escola.indexOf('3'));
 
 console.log(escola.substring(1));
-console.log(escola.substring(0, 3))
+console.log(escola.substring(0, 3));
+
+console.log('Escola '.concat(escola).concat("!"));
+console.log(escola.replace(3, 'e'));
