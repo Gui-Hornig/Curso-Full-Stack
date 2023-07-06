1a826c23a2b4cbfcdc73d99e31a56e97169d164b
Gui-hornig <guihornig@gmail.com>
2023-07-06 15:33:17 -0300

    Update numeros.js

--- JavaScript/Fundamento/numeros.js
+++ JavaScript/Fundamento/numeros.js
@@ -1,7 +1,8 @@
 const peso1 = 1.0;
-const peso2= Number('2.0');
+const peso2 = Number('2.0');
 
 console.log(peso1, peso2);
+
 console.log(Number.isInteger(peso1));
 console.log(Number.isInteger(peso2));
 
