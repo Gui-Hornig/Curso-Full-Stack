e683f685d0aefca253605fe04ebb40ed0ec7ec7b
Gui-hornig <guihornig@gmail.com>
2023-07-06 16:22:24 -0300

    Update strings.js

--- JavaScript/Fundamento/strings.js
+++ JavaScript/Fundamento/strings.js
@@ -8,7 +8,7 @@ console.log(escola.indexOf('3'));
 console.log(escola.substring(1));
 console.log(escola.substring(0, 3));
 
-console.log('Escola '.concat(escola).concat("!"));
+console.log('Escola '.concat(escola).concat("!")); // console.log('Escola' + escola)
 console.log(escola.replace(3, 'e'));
 
 console.log('Ana,Maria,Pedro'.split(',')) // gera um Array 
