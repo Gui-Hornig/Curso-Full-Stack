961698e60433f163c23bada39f815cc1e31d987d
Gui-hornig <guihornig@gmail.com>
2023-07-06 16:07:58 -0300

    Update strings.js

--- JavaScript/Fundamento/strings.js
+++ JavaScript/Fundamento/strings.js
@@ -2,4 +2,4 @@ const escola = "Cod3r";
 
 console.log(escola.charAt(4));
 console.log(escola.charAt(5));
-console.log(escola.charCodeAt(3));
+console.log(escola.charCodeAt(3)); // retorna o código relacionado a tabela UNICODE html
