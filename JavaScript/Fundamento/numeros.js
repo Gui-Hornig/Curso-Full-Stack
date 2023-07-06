61f4e438d004a66c357fb5ce712769036b8db70b
Gui-hornig <guihornig@gmail.com>
2023-07-06 15:32:53 -0300

    Update numeros.js

--- JavaScript/Fundamento/numeros.js
+++ JavaScript/Fundamento/numeros.js
@@ -5,7 +5,7 @@ console.log(peso1, peso2);
 console.log(Number.isInteger(peso1));
 console.log(Number.isInteger(peso2));
 
-const avaliacao1 = 9.871
-const avaliacao2 = 6.871
+const avaliacao1 = 9.871;
+const avaliacao2 = 6.871;
 
-const total = avaliacao1 * peso1 + avaliacao2 * peso2
+const total = avaliacao1 * peso1 + avaliacao2 * peso2;
