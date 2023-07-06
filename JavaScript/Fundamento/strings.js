8b3d05b461161f959bfa02ea2cee609b80746af9
Gui-hornig <guihornig@gmail.com>
2023-07-06 16:09:37 -0300

    Update strings.js

--- JavaScript/Fundamento/strings.js
+++ JavaScript/Fundamento/strings.js
@@ -3,4 +3,4 @@ const escola = "Cod3r";
 console.log(escola.charAt(4));
 console.log(escola.charAt(5));
 console.log(escola.charCodeAt(3)); // retorna o código relacionado a tabela UNICODE html, com relaçao ao digito "3" da palavra
-console.log()
+console.log(escola.indexOf('3'));
