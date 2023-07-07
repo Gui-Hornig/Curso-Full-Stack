bcad14c628cb5f2fb63594c5fe28e937793adfeb
Gui-hornig <guihornig@gmail.com>
2023-07-07 14:57:52 -0300

    Update funcao1.js

--- JavaScript/Fundamento/funcao1.js
+++ JavaScript/Fundamento/funcao1.js
@@ -1,3 +1,20 @@
-//Funcao sem retorno
 
-// Funcao e um trecho e codigo que foi dado um nome e pode reutilizar em algum momento, uma nova chamada
+
+// Funcao e um {bloco}trecho e codigo que foi dado um nome e pode reutilizar em algum momento, uma nova chamada
+
+//Funcao sem retorno
+function imprimirSoma (a, b) {
+    console.log(a + b);
+}
+imprimirSoma(2, 3);
+imprimirSoma(2); // Js aceita isso outras linguagens nao 
+imprimirSoma(2 ,10, 4, 5, 6, 6, 7) // Js aceita passar mais parametro ou nao passar, ja outras linguagens nao aceitam.
+
+// Funcao com retorno
+
+function soma(a, b= 1){
+    return a + b
+}
+console.log(soma (2, 3))
+console.log(soma(2))
+console.log(soma())
