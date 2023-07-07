6511e539aef4ceb45a3e678d15f1023c73c7d63a
Gui-hornig <guihornig@gmail.com>
2023-07-07 12:41:44 -0300

    Update Entendendo_Null_&_Undefined.js

--- JavaScript/Fundamento/Entendendo_Null_&_Undefined.js
+++ JavaScript/Fundamento/Entendendo_Null_&_Undefined.js
@@ -15,3 +15,6 @@ console.log(produto)
 produto.preco = undefined // evitar atribuir undefined
 console.log(!!produto.preco)
 //delete produto.preco 
+console.log(produto)
+
+produto.preco = null // sem preco
