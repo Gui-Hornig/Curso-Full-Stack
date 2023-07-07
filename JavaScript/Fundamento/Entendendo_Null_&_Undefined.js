2bd5f6625ab70d443fc5dead78a7f82dc7295212
Gui-hornig <guihornig@gmail.com>
2023-07-07 12:38:54 -0300

    Update Entendendo_Null_&_Undefined.js

--- JavaScript/Fundamento/Entendendo_Null_&_Undefined.js
+++ JavaScript/Fundamento/Entendendo_Null_&_Undefined.js
@@ -1,6 +1,16 @@
-let valor // nao iniciada
-console.log(valor)
+let valor; // nao iniciada
+console.log(valor);
 
 valor = null // ausencia de valor 
-console.log(valor)
+console.log(valor);
 //console.log(valor.toString()) // ERRO!
+
+const produto = {};
+console.log(produto.preco);
+console.log(produto);
+
+produto. preco = 3.5;
+console.log(produto)
+
+produto.preco = undefined // evitar atribuir undefined
+console.log()
