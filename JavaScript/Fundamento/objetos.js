ac998b3e79051f8018a639e54d05ec3843b1a2b3
Gui-hornig <guihornig@gmail.com>
2023-07-07 12:06:53 -0300

    Update objetos.js

--- JavaScript/Fundamento/objetos.js
+++ JavaScript/Fundamento/objetos.js
@@ -2,4 +2,9 @@ const prod1 = {} // criacao  de forma literal
 prod1.nome = 'celular ultra mega' // atribuir valores dinamicamente
 prod1.preco = 4998.90
 prod1.desconto = '%10'
+
 console.log(prod1)
+
+const prod2 = { // criando objeto de forma literal e ja atribuindo os valores
+
+}
