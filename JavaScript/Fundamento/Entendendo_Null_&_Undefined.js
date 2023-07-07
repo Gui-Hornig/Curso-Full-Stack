5a9577454228a4048482af7e9cf4a48be61171e2
Gui-hornig <guihornig@gmail.com>
2023-07-07 12:53:41 -0300

    Update Entendendo_Null_&_Undefined.js

--- JavaScript/Fundamento/Entendendo_Null_&_Undefined.js
+++ JavaScript/Fundamento/Entendendo_Null_&_Undefined.js
@@ -1,3 +1,28 @@
+
+const a = {name:'teste'} // criou um objeto com atributo 
+console.log(a)
+
+const b = a // pegando o valor de A e atribuindo para B. O que acoantece? 
+// A variavel A nao tem o conteudo do objeto ( {name:'teste'}  ) ela tem o endereco apontando para o objeto.
+// Quando faz a atribuicao "const b = a" vc passa o endereco da memoria para a variavel "B"
+// Assim elas passam a apontar para o mesmo endereco na memoria de tal farma que alterando uma altera as 2 EX:
+b.name = 'opa'
+console.log(a)
+// as duas estao com a mesma referencia do objeto na memoria
+// outros EXs: 
+
+let c = 3
+let d = c
+d++ // incrementa uma uinidade ao valor da variavel 'D'
+console.log(d)
+console.log(c)
+
+
+
+
+
+//___________________________________________________________________
+
 let valor; // nao iniciada
 console.log(valor);
 
