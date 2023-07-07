03a46ea5da4dc36c9ac3bcef0623186894fa36ee
Gui-hornig <guihornig@gmail.com>
2023-07-07 16:18:14 -0300

    Update formatar_e_localizar.js

--- JavaScript/Fundamento/formatar_e_localizar.js
+++ JavaScript/Fundamento/formatar_e_localizar.js
@@ -1,4 +1,6 @@
 var n1 = 1545.50
-console.log(n1)
+//console.log(n1)
 //n1.toFixed(2)
-n1.toFixed(2).replace('.' , ',')
+//n1.toFixed(2).replace('.' , ',')
+ n1.formattedNumber('pt-BR', {style: 'currency', currency: "BRL"})
+ console.log(n1)
