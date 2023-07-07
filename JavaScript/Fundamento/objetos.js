02c38ae01ee4c0480d47301aab5d0645552848d2
Gui-hornig <guihornig@gmail.com>
2023-07-07 12:10:05 -0300

    Update objetos.js

--- JavaScript/Fundamento/objetos.js
+++ JavaScript/Fundamento/objetos.js
@@ -4,7 +4,13 @@ prod1.preco = 4998.90
 prod1.desconto = '%10'
 
 console.log(prod1)
+// objeto e uma colecao chave valor
+// criando objeto de forma literal e ja atribuindo os valores
+const prod2 = { 
+    nome: 'Camisa Polo',
+    preco: 79,90,
+    obj{
+        tecido:'Malha algodao',
 
-const prod2 = { // criando objeto de forma literal e ja atribuindo os valores
-
+    } 
 }
