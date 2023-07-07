169a79298f3e7b5b2a051a029e1153a921726752
Gui-hornig <guihornig@gmail.com>
2023-07-07 20:39:13 -0300

    Update funcao2.js

--- JavaScript/Fundamento/funcao2.js
+++ JavaScript/Fundamento/funcao2.js
@@ -5,3 +5,16 @@
 
     imprimirSoma(2, 3)
  }
+
+ //Armazenando uma funcao Arrow em uma variavel
+
+ const soma = (a, b) => {   // funcao COM {} podem excutar varias sentencas de codigo
+    return a + b
+ }
+ console.log(soma(2, 3))
+
+ // Retorno Implicito
+
+ const subtracao = (a, b) => a - b     // funcao SEM os {} executam apenas uma lihha de codigo
+ console.log(subtracao(2, 3))
+
