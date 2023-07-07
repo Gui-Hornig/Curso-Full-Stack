71c8cf07f1973c215f1e2c515eef2b5b43274c5c
Gui-hornig <guihornig@gmail.com>
2023-07-07 11:28:05 -0300

    Update array.js

--- JavaScript/Fundamento/array.js
+++ JavaScript/Fundamento/array.js
@@ -1,6 +1,18 @@
 const valores = [7.7 , 8.9 , 6.3 , 9.2];
 console.log(valores [0], valores[3]);
-console.log(valores[4])
-
-valores [4] = 10;
 console.log(valores[4]);
+
+valores [10] = 10; // o JS completa automaticamente dos arrays com espacos vazios
+console.log(valores); 
+ 
+valores [4] = 10 ; // os arrays dos JS sao maleaveis 
+console.log(valores)
+console.log(valores [4]);
+
+console.log(valores.length); // retorna a quantidade de valores do array
+
+valores.push({id:3}, false, null, 'teste');
+console.log(valores);
+
+
+
