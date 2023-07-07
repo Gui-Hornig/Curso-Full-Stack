44a7e23f34c5699663423c2a87f145f1b5550964
Gui-hornig <guihornig@gmail.com>
2023-07-07 10:54:36 -0300

    Update booleanos.js

--- JavaScript/Fundamento/booleanos.js
+++ JavaScript/Fundamento/booleanos.js
@@ -1,2 +1,16 @@
 let isAtivo = false
 console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo) //negação 
+
+console.log('os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
