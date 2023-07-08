6f4d31e8d2615815b2a50146a359a46b159d094d
Gui-hornig <guihornig@gmail.com>
2023-07-07 21:26:39 -0300

    Create UsandoVarEmLoop.js

---
+++ JavaScript/Fundamento/UsandoVarEmLoop.js
@@ -0,0 +1,10 @@
+for (var i = 0; i <10; i++){
+    console.log(i)
+}
+console.log('1=', i)
+
+/* Laco de repeticao
+* for(var i = 0; Primeira parte: Declaracao da variavel ('var', 'let', 'const')
+* i<10; i++ Segunda parte e a condicao que ele ira ficar repetindo o codigo
+* {} Terceira parte o incremento (bloco de codigo) 
+*/
