d4013f6525c4a4c9d7ef202e6bbfb781e5fccfba
Gui-hornig <guihornig@gmail.com>
2023-07-08 03:42:24 -0300

    Update OperadoresRelacionais.js

--- JavaScript/Fundamento/OperadoresRelacionais.js
+++ JavaScript/Fundamento/OperadoresRelacionais.js
@@ -15,3 +15,12 @@ console.log ('06)', 3 > 2)
 console.log ('07)', 3 <= 2)
 
 console.log ('08', 3>=2)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log ('09)', d1 === d2)
+console.log ('10)', d1 == d2)
+console.log ('11)', d1.getTime () === d2)
+
+console.log ('12', undefined == null)
+console.log ('13', underined === nuell)
