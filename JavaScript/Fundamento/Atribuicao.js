8859a1ee4af0a8f465db28ad08d1f55d9424a409
Gui-hornig <guihornig@gmail.com>
2023-07-08 02:13:24 -0300

    Update Atribuicao.js

--- JavaScript/Fundamento/Atribuicao.js
+++ JavaScript/Fundamento/Atribuicao.js
@@ -3,3 +3,12 @@ let b = 3
 
 b += a  // equevale b = b + a //atribuicao aditiva
 console.log(b)
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = / 2
+console.log (b)
