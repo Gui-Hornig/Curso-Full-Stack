d2537582d8ea890b108dbd1827783f4b191729db
Gui-hornig <guihornig@gmail.com>
2023-07-07 21:30:15 -0300

    Create UsandoLetEmLoop.js

---
+++ JavaScript/Fundamento/UsandoLetEmLoop.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+console.log('i =', i) 
+
+//Nesse ex. podemos observar que usando 'Let' nao foi possivel imprimir o valor fora do escopo {} bloco de codigo
