1942baea2f24f53d3389dfcec5879e3c80a3fd5a
Gui-hornig <guihornig@gmail.com>
2023-07-07 21:51:32 -0300

    Create hoisting.js

---
+++ JavaScript/Fundamento/hoisting.js
@@ -0,0 +1,16 @@
+// traducao seria "Ica ou jogar para cima" 
+// Faz exatamente isso no codigo 
+
+ console.log('a=', a) // declarada com "var" a variave sofre esse efeito de ser jogada para cima pelo interpretador
+ var a = 2  
+ console.log('a=', a)
+
+ function teste(){
+    console.log('a=', a) // declarada com "var" a variave sofre esse efeito de ser jogada para cima pelo interpretador
+    var a = 2  
+    console.log('a=', a)
+ }
+console.log(a)
+
+//Nao faz sentido fazer isso pois o interpretador faz algumas manipulacoes 
+//SEMPRE declare as variaveis antes ou dentro don seu respectivo escopo
