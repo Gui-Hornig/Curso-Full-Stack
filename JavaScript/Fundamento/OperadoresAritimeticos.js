c69e72760dde95d1319e16fd143788bbf5e38526
Gui-hornig <guihornig@gmail.com>
2023-07-08 03:27:03 -0300

    Create OperadoresAritimeticos.js

---
+++ JavaScript/Fundamento/OperadoresAritimeticos.js
@@ -0,0 +1,10 @@
+const [a,b,c,d] = [3,5,1,15]
+
+const soma = a + b + c + d
+console.log (soma)
+const subtracao = d - b
+const divisao = d/a
+const multiplica = a * b
+const modulo = a % 2
+
+console.log (soma, subtracao, multiplica, divisao, modulo)
