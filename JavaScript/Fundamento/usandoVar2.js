05c392a1593688d4414e38318a71ba68dbc149f8
Gui-hornig <guihornig@gmail.com>
2023-07-07 21:05:31 -0300

    Create usandoVar2.js

---
+++ JavaScript/Fundamento/usandoVar2.js
@@ -0,0 +1,11 @@
+var numero = 1
+{
+        var numero =2
+        console.log ('dentro=', numero)
+}
+console.log ('fora =', numero) 
+
+/*Nesse ex. tento a mesma variavel no mesmo escopo ela sobrescrevel a primeira e foi impressa 2x
+*Isso gera muito erro pois outros colegas de trabalho podem declar a mesma variavel 
+*De preferencia para sempre declar variaveis dentro do seu escopo
+*/
