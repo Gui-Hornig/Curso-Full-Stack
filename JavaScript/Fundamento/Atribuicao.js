d5f8857cc3d6b587e1c6684909b9aa3045d61dc1
Gui-hornig <guihornig@gmail.com>
2023-07-08 02:16:10 -0300

    Update Atribuicao.js

--- JavaScript/Fundamento/Atribuicao.js
+++ JavaScript/Fundamento/Atribuicao.js
@@ -12,3 +12,6 @@ console.log(b)
 
 b /= 2 // b = / 2
 console.log (b)
+
+b %= 2 // b = b % 2  // resto da divisao, se for um valor par o resultato e zero e se for um valor impar e 1
+console.log (b)
