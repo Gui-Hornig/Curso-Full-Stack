79f72f8d1b5bb7e3b255013fd160655d871b4f59
Gui-hornig <guihornig@gmail.com>
2023-07-09 02:18:07 -0300

    Update If1.js

--- JavaScript/EsuturasDeControle/If1.js
+++ JavaScript/EsuturasDeControle/If1.js
@@ -1,6 +1,7 @@
 function soBoaNoticia(nota){
     if (nota >= 7) {
-        console.log ('Aprovado' + nota)
+        console.log ('Aprovado com ' + nota)
     }
 }
-console.log(nota(7))
+soBoaNoticia(8.1)
+soBoaNoticia(6)
