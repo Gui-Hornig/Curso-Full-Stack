944dde5eaac0c95e17dcb892abc1d0ae2e0b14bd
Gui-hornig <guihornig@gmail.com>
2023-07-09 02:26:10 -0300

    Update If1.js

--- JavaScript/EsuturasDeControle/If1.js
+++ JavaScript/EsuturasDeControle/If1.js
@@ -5,3 +5,22 @@ function soBoaNoticia(nota){
 }
 soBoaNoticia(8.1)
 soBoaNoticia(6)
+
+function seForVerdadeEuFalo (valor) {
+    if (valor) {
+        console.log('E verdade... ' + valor)
+    }
+}
+
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo('?')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1, 2])
+seForVerdadeEuFalo({})
