8501415ece20b93a9b5feecc1158a83a8dbe04c5
Gui-hornig <guihornig@gmail.com>
2023-07-09 01:32:51 -0300

    Update OperadoresTernarios.js

--- JavaScript/Fundamento/OperadoresTernarios.js
+++ JavaScript/Fundamento/OperadoresTernarios.js
@@ -3,3 +3,8 @@
 const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
 console.log(resultado(7))
 console.log(resultado(6))
+
+const resultado2 = nota => {
+    return nota >= 7 ? 'Aprovado' : 'Reprovado'
+}
+console.log(resultado2(7))
