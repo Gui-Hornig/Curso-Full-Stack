cac14b92c2df8ba030d2a877dc5281b0c2ea0bc7
Gui-hornig <guihornig@gmail.com>
2023-07-09 02:01:11 -0300

    Create tratamentoDeErro.js

---
+++ JavaScript/Fundamento/tratamentoDeErro.js
@@ -0,0 +1,26 @@
+function tratarErroELancar(erro){
+    // throw new Error('Codigo Bugado')
+    // throw 10
+    // throw true
+    // throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado (obj) {
+    try{ 
+    console.log (obj.name.toUpperCaase() + '!!!')
+    } catch (e){
+       tratarErroELancar(e) 
+    }
+}
+const obj = { nome:'Roberto'}
+imprimirNomeGritado(obj)
+
+// 'try' e um bloco de codigo que colocamos em codigos que possivelmente podem gerar erro.
+// Caso gere esse erro "esperado" o  codigo cai para o bloco 'cath', assim vc pode exibr uma msg ao usuario
+
+console.log(new Date )
