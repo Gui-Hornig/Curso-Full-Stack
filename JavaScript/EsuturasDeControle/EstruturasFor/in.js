964b591e31e8e08f394d9c7d71fd8691ecd9f0a5
Gui-hornig <guihornig@gmail.com>
2023-07-09 03:52:51 -0300

    Update in.js

--- JavaScript/EsuturasDeControle/EstruturasFor/in.js
+++ JavaScript/EsuturasDeControle/EstruturasFor/in.js
@@ -1,6 +1,6 @@
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
 
-for (i in notas) {
+for (let i in notas) {
     console.log(i, notas[i])
 }
 
@@ -10,6 +10,6 @@ const pessoa = {
     idade: 29,
     peso: 64
 }
-for (atributo in pessoa) {
+for (let atributo in pessoa) {
     console.log(`${atributo} = ${pessoa[atributo]}`)
 }
