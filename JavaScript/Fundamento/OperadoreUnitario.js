fa3ba00c7f1d594cf3b3b12bfe8c3bf6ae73d926
Gui-hornig <guihornig@gmail.com>
2023-07-08 21:51:28 -0300

    Update OperadoreUnitario.js

--- JavaScript/Fundamento/OperadoreUnitario.js
+++ JavaScript/Fundamento/OperadoreUnitario.js
@@ -3,3 +3,8 @@ let num2 = 2
 
 num1++ //acrecenta uma unidade
 console.log(num1)
+
+--num1 //Subtai TEM PRIORIDADE NA ORDEM DE PRECEDENCIA escrito assim: --num1 ou ++num2
+console.log(num1)
+
+console.log(++num1 === num2--)
