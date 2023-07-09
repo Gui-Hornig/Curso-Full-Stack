b8c019b606d32b1dabbe066a3d8de4e5db2210cd
Gui-hornig <guihornig@gmail.com>
2023-07-09 03:25:31 -0300

    Update RepeticaoWhile.js

--- JavaScript/EsuturasDeControle/RepeticaoWhile.js
+++ JavaScript/EsuturasDeControle/RepeticaoWhile.js
@@ -1,2 +1,11 @@
 // Estruturas indeterminadas de vezes
- 
+ function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre ( -1, 10)
+    console.log('Opcao Escolhida foi ${opcao}.')
+}
