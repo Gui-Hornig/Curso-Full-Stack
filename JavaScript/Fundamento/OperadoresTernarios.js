73b2edb80bb992d20c79c616c07b4b5aca866668
Gui-hornig <guihornig@gmail.com>
2023-07-09 01:30:02 -0300

    Update OperadoresTernarios.js

--- JavaScript/Fundamento/OperadoresTernarios.js
+++ JavaScript/Fundamento/OperadoresTernarios.js
@@ -1,5 +1,5 @@
 // Opradores ternarios sao dividos em 3 expressoes, PRIMEIRA parte e uma expressao relacional se for TRUE ela retorna EX. 'Aprovado' se for falso, retona Ex.'Reprovado'
 
-const resultado = nota => 7 ? 'Aprovado' : 'Reprovado'
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
 console.log(resultado(7))
 console.log(resultado(6))
