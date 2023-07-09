713ff0dd1f123aa0ad75082de88f0e112d197eca
Gui-hornig <guihornig@gmail.com>
2023-07-09 03:45:51 -0300

    Update EstruturaFor1.js

--- JavaScript/EsuturasDeControle/EstruturaFor1.js
+++ JavaScript/EsuturasDeControle/EstruturaFor1.js
@@ -9,6 +9,6 @@ for (let i = 1; i <= 10 ; i++){
 } 
 
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
-for(i = 0; i < notas.length; i++){
+for(let i = 0; i < notas.length; i++){
     console.log(`notas = ${notas[i]}`)
 }
