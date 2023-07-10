0f59c9bd0ab376cb65748d0ea7d5266eadadc4b8
Gui-hornig <guihornig@gmail.com>
2023-07-09 23:28:04 -0300

    Update ParametroPadrao.js

--- JavaScript/Funcoes/ParametroPadrao.js
+++ JavaScript/Funcoes/ParametroPadrao.js
@@ -21,3 +21,8 @@ console.log (soma2())
 console.log (soma2(), soma2(3), soma2(1, 2, 3), soma2(0,0,0))
 
 //Valor padrao do Es2015
+
+function soma3( a = 1, b = 1, c = 1) {
+    return a + b + c
+}
+console.log (soma3(), soma3(3), soma3(1, 2, 3), soma3(0,0,0))
