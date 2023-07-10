8f9ca4cdd7f16a7e1200b9f24f605969488a53d8
Gui-hornig <guihornig@gmail.com>
2023-07-10 13:50:07 -0300

    Update Call&Apply.js

--- JavaScript/Funcoes/Call&Apply.js
+++ JavaScript/Funcoes/Call&Apply.js
@@ -20,5 +20,5 @@ const carro = {preco : 49990, desc: 0.20}
 console.log (getPreco.call(carro))
 console.log (getPreco.apply(carro))
 
-console.log(getPreco.call(carro, 0.17, '$'))
-console.log(getPreco.apply (carro, [0.20, '%']))
+console.log(getPreco.call(carro, 0.17, '$')) // Parametros dentro da funcao 
+console.log(getPreco.apply (carro, [0.20, '%']))  // Parametros dentro do array 
