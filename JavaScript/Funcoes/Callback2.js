9c75a6e096ff7e62b2740518cf9c2fba2861cf28
Gui-hornig <guihornig@gmail.com>
2023-07-10 11:45:12 -0300

    Update Callback2.js

--- JavaScript/Funcoes/Callback2.js
+++ JavaScript/Funcoes/Callback2.js
@@ -2,11 +2,23 @@ const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
 
 // sem callback
 
-let notasBaixas = []
+const notasBaixas1 = []
 for (let i in notas) {
-    if (notas [i] > 7 ) {
-        notasBaixas.push(notas[i])
+    if (notas [i] < 7 ) {
+        notasBaixas1.push(notas[i])
     }
 }
 
-console.log(notas)
+console.log(notasBaixas1)
+
+// Com CallBack
+
+const notasBaixas2 = notas.filter(function (nota) {
+    return nota < 7
+})
+console.log (notasBaixas2)
+
+// Com arrow
+
+const notasBaixas3 = notas.filter(notas => notas < 7)
+console.log(notasBaixas3)
