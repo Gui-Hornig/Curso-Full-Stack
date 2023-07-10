82d0b3bf6635618ee4549314af2902767a2c4b78
Gui-hornig <guihornig@gmail.com>
2023-07-10 11:36:10 -0300

    Update Callback1.js

--- JavaScript/Funcoes/Callback1.js
+++ JavaScript/Funcoes/Callback1.js
@@ -1,1 +1,12 @@
 // chamar de volta
+
+const fabricantes  = ["Mercedes", "Audi", "BMW"]
+
+function imprimir (nome, indice) {
+    console.log (`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(function(fabricante){
+    console.log(fabricante)
+})
