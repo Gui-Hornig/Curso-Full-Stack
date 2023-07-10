0a4584b295ba52ced2dbe35ed2b2de64d7f98ade
Gui-hornig <guihornig@gmail.com>
2023-07-09 23:49:35 -0300

    Update ThisEBind.js

--- JavaScript/Funcoes/ThisEBind.js
+++ JavaScript/Funcoes/ThisEBind.js
@@ -1,6 +1,13 @@
 const pessoa = {
-    saudadao: 'Bom dia!',
+    saudacao: 'Bom dia!',
     falar () {
-        
+        console.log(this.saudacao)
     }
 }
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() // conflito entre paadgmas : funcional e orientcao a Objeto
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
