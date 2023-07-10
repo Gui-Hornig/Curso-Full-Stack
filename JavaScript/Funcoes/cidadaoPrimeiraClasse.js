8837bc5d305ad6b65f75c3777e1b88e0222a2ebc
Gui-hornig <guihornig@gmail.com>
2023-07-09 23:00:16 -0300

    Update cidadaoPrimeiraClasse.js

--- JavaScript/Funcoes/cidadaoPrimeiraClasse.js
+++ JavaScript/Funcoes/cidadaoPrimeiraClasse.js
@@ -27,3 +27,15 @@ function run(fun) {
     fun()
 }
 run( function () { console.log('Executanto...') })
+
+// Pode retornar uma funcao / conter
+
+function soma (a, b) {
+    return  function (c) {
+        console.log(a + b + c)
+    }
+}
+
+soma (2, 3)(4)
+const cincoMais = soma (2, 3)
+cincoMais (3)
