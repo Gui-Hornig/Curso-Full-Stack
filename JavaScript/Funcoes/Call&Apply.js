659fb250251f3a8a246486dd06d49d3db7f2eafd
Gui-hornig <guihornig@gmail.com>
2023-07-10 13:41:50 -0300

    Update Call&Apply.js

---
+++ JavaScript/Funcoes/Call&Apply.js
@@ -0,0 +1,10 @@
+function getPreco(imposto = 0, moeda = 'R$') {
+    return `${moeda} ${this.preco * (1 - this.desc) * (1 + imposto)}`
+}
+
+const produto = {
+    nome: 'Notebook'
+    preco: 4589,
+    desc: 0.15,
+    getPreco
+}
