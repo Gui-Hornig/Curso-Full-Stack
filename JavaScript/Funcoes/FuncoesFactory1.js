3de08fc70a34c305a82d21dd7d92e5721cab5ca5
Gui-hornig <guihornig@gmail.com>
2023-07-10 13:01:25 -0300

    Update FuncoesFactory1.js

---
+++ JavaScript/Funcoes/FuncoesFactory1.js
@@ -0,0 +1,9 @@
+// Factory simples (funcao que retorna um objeto)
+function criarPessoa () {
+    return {
+        noma: 'Ana',
+        sobrenome: 'Silva',
+        idade: '344'
+    }
+}
+console.log(criarPessoa())
