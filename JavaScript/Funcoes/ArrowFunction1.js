b0c0b837c8591513d5bc1eef270ed1b163df4e20
Gui-hornig <guihornig@gmail.com>
2023-07-10 00:18:11 -0300

    Update ArrowFunction1.js

--- JavaScript/Funcoes/ArrowFunction1.js
+++ JavaScript/Funcoes/ArrowFunction1.js
@@ -8,3 +8,11 @@ dobro = (a) => {
 // Reescrevendo, tirando os () caso tenha um unico parametro e pode tirar as {} vc tem um retorno implicita, funcoes com 1 linha
 dobro = a => 2 * a
 console.log(dobro(3))
+
+let ola = function () {
+    return 'Ola'
+}
+
+ola = () => 'Ola'
+ola = _ => 'Ola' // Possui um parametro '_'
+console.log(ola())
