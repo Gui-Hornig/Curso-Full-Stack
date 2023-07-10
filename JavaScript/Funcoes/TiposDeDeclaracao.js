1a737ab7187473a4ac3fc564d6189d1ad0749343
Gui-hornig <guihornig@gmail.com>
2023-07-10 12:24:36 -0300

    Update TiposDeDeclaracao.js

---
+++ JavaScript/Funcoes/TiposDeDeclaracao.js
@@ -0,0 +1,22 @@
+// function declaration 
+console.log (soma(3, 4))
+
+function soma (x, y) {
+    return x + y
+}
+
+// function expression 
+const sub = function (x, y) {
+    return x - y
+}
+
+const soma2 = function (x, y) {
+    return x + y
+}
+console.log(soma2(2 , 4))
+
+// named function expression 
+
+const mult = function mult (x, y) {
+    return x * y
+}
