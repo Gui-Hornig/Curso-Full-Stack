c025645dd6681b6273d01d78e84dd486750b9943
Gui-hornig <guihornig@gmail.com>
2023-07-10 00:32:42 -0300

    Update ArrowFunction3.js

--- JavaScript/Funcoes/ArrowFunction3.js
+++ JavaScript/Funcoes/ArrowFunction3.js
@@ -2,3 +2,11 @@ let comparaComThis = function (param) {
     console.log (this === param)
 }
 comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComArrow = param => console.log(this === param)
+comparaComArrow (global)
