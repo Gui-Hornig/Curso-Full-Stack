53aea8e89573ac1d8626036aa9068dd6414d3c45
Gui-hornig <guihornig@gmail.com>
2023-07-11 15:04:15 -0300

    Create ObjetoRevisao.js

---
+++ OrientacaoObjeto/ObjetoRevisao.js
@@ -0,0 +1,31 @@
+// Colecao Dinamica de pares chave /valor 
+
+const produto  = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo = 'A4',
+    valor : 89000,
+    proprietario: {
+        nome : 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }]
+    calcularValorSeguro: function () {
+        // ...
+    }
+}
