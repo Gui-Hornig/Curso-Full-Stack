21b89d45dc893871fb63884dfddb7ca560c5e46b
Gui-hornig <guihornig@gmail.com>
2023-07-11 15:56:50 -0300

    Update ObjetosConstantes.js

--- OrientacaoObjeto/ObjetosConstantes.js
+++ OrientacaoObjeto/ObjetosConstantes.js
@@ -7,4 +7,11 @@ console.log(pessoa)
 // pessoa -> outro endereco -> {...}
 // pessoa = { nome: 'Ana'}
 
-Object.freeze(pessoa)
+Object.freeze(pessoa) // congelar o Objeto, nao consegue mais modificar/ Torna Object constante
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua Abc'
+delete pessoa.nome 
+
+console.log(pessoa.nome)
+console.log(pessoa)
