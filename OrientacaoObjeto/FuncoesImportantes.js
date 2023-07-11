84a14336f0a11f67073b510a41713718010aa5c8
Gui-hornig <guihornig@gmail.com>
2023-07-11 17:04:58 -0300

    Update FuncoesImportantes.js

--- OrientacaoObjeto/FuncoesImportantes.js
+++ OrientacaoObjeto/FuncoesImportantes.js
@@ -11,3 +11,13 @@ console.log(Object.entries(pessoa) )
 Object.entries(pessoa).forEach(([chave, valor]) => {
     console.log(`${chave}: ${valor}`)
 })
+
+Object.defineProperty(pessoa, 'dataNascimento') ({
+    enumerable: true,
+    writable: false,
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
