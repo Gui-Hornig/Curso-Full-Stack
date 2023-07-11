e10da0e8d9ef6ad3525d3936dfaa089ec5f56939
Gui-hornig <guihornig@gmail.com>
2023-07-11 17:29:20 -0300

    Update herancas1.js

--- OrientacaoObjeto/herancas1.js
+++ OrientacaoObjeto/herancas1.js
@@ -10,3 +10,8 @@ const volvo = {
 console.log (ferrari.__proto__)
 console.log (ferrari.__proto__ === Object.prototype)
 console.log (volvo.__proto__ === Object.prototype)
+console.log (Object.prototype.__proto__ === null)
+
+function MeuObjeto() {}
+    console.log (typeof Object, typeof MeuObjeto)
+    console.log (Object.prototype, MeuObjeto.prototype)
