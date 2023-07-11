6d86e0e228dd25582e5127f227ae907c2ff751cc
Gui-hornig <guihornig@gmail.com>
2023-07-11 17:13:01 -0300

    Update FuncoesImportantes.js

--- OrientacaoObjeto/FuncoesImportantes.js
+++ OrientacaoObjeto/FuncoesImportantes.js
@@ -27,4 +27,8 @@ console.log(Object.keys(pessoa))
 const dest = {a: 1}
 const o1 = {b: 2}
 const o2 = {c:3, a:4}
-const obj = Object.assign(dest, o1, 02,)
+const obj = Object.assign(dest, o1, o2)
+
+Object.freeze (obj)
+obj.c = 1234
+console.log(obj)
