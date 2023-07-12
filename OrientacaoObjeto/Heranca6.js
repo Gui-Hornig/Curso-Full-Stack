9fc5f81db33a6d0866537a2a532406e880cb9d38
Gui-hornig <guihornig@gmail.com>
2023-07-12 16:05:50 -0300

    Update Heranca6.js

--- OrientacaoObjeto/Heranca6.js
+++ OrientacaoObjeto/Heranca6.js
@@ -6,3 +6,16 @@ function Aula(nome, videoId){
 const aula1 = new Aula ('Bem Vindo', 123)
 const aula2 = new Aula ('Ate Breve', 456)
 console.log (aula1, aula2)
+
+//simular o new
+
+function novo (f, ...params) {
+    const obj = {}
+    obj.proto__ = f.prototype
+    f.apply(obj, params)
+    return obj
+}
+
+const aula3 = novo(Aula, 'Bem Vindo', 123)
+const aula4 = novo(Aula, 'Ate breve', 456)
+console.log(aula3, aula4)
