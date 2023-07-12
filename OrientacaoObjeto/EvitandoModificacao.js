fe49b8f46a5cdea60c5f7c6db78c9d58dd1eb056
Gui-hornig <guihornig@gmail.com>
2023-07-12 16:12:51 -0300

    Update EvitandoModificacao.js

--- OrientacaoObjeto/EvitandoModificacao.js
+++ OrientacaoObjeto/EvitandoModificacao.js
@@ -1,2 +1,12 @@
 // Object.preventExtensions
 
+const produto = Object.preventExtensions({
+    nome : 'Qualquer', preco: 1.99, tag: 'promocao'
+})
+
+console.log('Extensivel', Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar branca'
+delete produto.tag
+console.log(produto)
