aa67ea32245ca9be00f6a9dcaffa7b14479364ad
Gui-hornig <guihornig@gmail.com>
2023-07-12 17:04:55 -0300

    Update Classe2.js

---
+++ OrientacaoObjeto/Classe2.js
@@ -0,0 +1,13 @@
+class Avo {
+    constructor (sobrenome) {
+        this.sobrenome = sobrenome
+    }
+}
+
+class Pai extends Avo {
+    constructor (sobrenome, profissao = 'Professor') {
+        super (sobrenome)
+        this.profissao = profissao
+    }
+}
+
