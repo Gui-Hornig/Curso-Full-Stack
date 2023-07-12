9d04433c8aa3c62a93ccb50547d423c900a5af99
Gui-hornig <guihornig@gmail.com>
2023-07-12 15:30:14 -0300

    Update Heranca4.js

--- OrientacaoObjeto/Heranca4.js
+++ OrientacaoObjeto/Heranca4.js
@@ -5,3 +5,10 @@ const obj1 = new MeuObjeto
 const obj2 = new MeuObjeto
 console.log(obj1.__proto__ === obj2.__proto__)
 console.log(obj2.__proto__ === obj1.__proto__)
+
+MeuObjeto.prototype.nome = 'Anoniomo'
+MeuObjeto.prototype.falar = function() {
+    console.log (`Bom dia! Meu nome e ${this.nome}!`)
+}
+
+obj1.falar()
