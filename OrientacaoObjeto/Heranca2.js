6d69526c478c446b4f8bd4451bf85ef9749bfb3a
Gui-hornig <guihornig@gmail.com>
2023-07-12 14:50:44 -0300

    Update Heranca2.js

--- OrientacaoObjeto/Heranca2.js
+++ OrientacaoObjeto/Heranca2.js
@@ -5,3 +5,18 @@ const avo = {attr1 : 'A'}
 const pai = {__proto__: avo, attr2: 'B', attr3: '3'}
 const filho = {__photo__: pai, attr3: 'C'}
 console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
+
+const = carro = {
+    velAtual: 0,
+    velMax: 200,
+    acelearMaris(delta){
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual += delta
+        }else {
+            this.velAtual = this.velMax
+        }
+    },
+    status(){
+        
+    }
+}
