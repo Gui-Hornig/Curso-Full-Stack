29ebb00fc24aa11d6451b6223501e7df38c9c9a8
Gui-hornig <guihornig@gmail.com>
2023-07-12 15:36:06 -0300

    Update Heranca4.js

--- OrientacaoObjeto/Heranca4.js
+++ OrientacaoObjeto/Heranca4.js
@@ -12,3 +12,18 @@ MeuObjeto.prototype.falar = function() {
 }
 
 obj1.falar()
+
+obj2.nome = 'Rafael'
+obj2.falar()
+
+const obj3 = {}
+obj3.__proto__ = MeuObjeto.prototype
+obj3.nome = 'Obj3'
+obj3.falar()
+
+//Resumindo
+
+console.log((new MeuObjeto).__proto__ === MeuObjeto.prototype)
+console.log(MeuObjeto.__proto__ === Function.prototype)
+console.log(Function.prototype.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === undefined)
