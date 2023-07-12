72e9be9f51fde8bfcaae6e73e5340db61c778d21
Gui-hornig <guihornig@gmail.com>
2023-07-12 17:20:48 -0300

    Update Array.js

--- Array/Array.js
+++ Array/Array.js
@@ -1,1 +1,10 @@
 console.log(typeof Array, typeof new Array, typeof [])
+
+let aprovados = new Array ('Bia', 'Carlos', 'Ana')
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
