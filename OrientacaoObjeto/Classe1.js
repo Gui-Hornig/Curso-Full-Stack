f4aa3ce48ea0cd12612dade1f2241a5ff4ab0674
Gui-hornig <guihornig@gmail.com>
2023-07-12 16:56:52 -0300

    Update Classe1.js

--- OrientacaoObjeto/Classe1.js
+++ OrientacaoObjeto/Classe1.js
@@ -1,37 +1,35 @@
 class Lancamento {
     constructor(nome = 'Generico', valor = 0) {
-        this.nome = nome
-        this.valor = valor
+      this.nome = nome;
+      this.valor = valor;
     }
-}
-
-class CicloFinanceiro {
+  }
+  
+  class CicloFinanceiro {
     constructor(mes, ano) {
-        this.mes = mes
-        this.ano = ano
-        this.Lancamento = []
+      this.mes = mes;
+      this.ano = ano;
+      this.lancamentos = [];
     }
-
-
+  
     addLancamentos(...lancamentos) {
-        lancamentos.forEach(l => this.lancamentos.push(l))
+      lancamentos.forEach(l => this.lancamentos.push(l));
     }
-
+  
     sumario() {
-        let valorConsolidado = 0
-        this.lancamentos.forEach(l => {
-            valorConsolidado += l.valor
-        })
-
-        return valorConsolidado
-
+      let valorConsolidado = 0;
+      this.lancamentos.forEach(l => {
+        valorConsolidado += l.valor;
+      });
+  
+      return valorConsolidado;
     }
-
-}
-
-const salario = new Lancamento('Salario', 45000)
-const contaDeluz = new Lancamento('Luz', -220)
-
-const contas = new CicloFinanceiro(6, 2018)
-contas.addLancamentos(salario, contaDeluz)
-console.log(contas.sumario())
+  }
+  
+  const salario = new Lancamento('Salario', 45000);
+  const contaDeluz = new Lancamento('Luz', -220);
+  
+  const contas = new CicloFinanceiro(6, 2018);
+  contas.addLancamentos(salario, contaDeluz);
+  console.log(contas.sumario());
+  
