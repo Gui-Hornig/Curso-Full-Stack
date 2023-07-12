25e2f8a62e873fcb5970e66791f1cdcb2450e338
Gui-hornig <guihornig@gmail.com>
2023-07-12 15:20:15 -0300

    Update Heranca3.js

--- OrientacaoObjeto/Heranca3.js
+++ OrientacaoObjeto/Heranca3.js
@@ -12,4 +12,10 @@ console.log(filha2.nome)
 filha2.nome = 'Carla'
 console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`)
 
-conms
+console.log(Object.keys(filha1))
+console.log(Object.keys(filha2))
+
+for(let key in filha2) {
+    filha2.hasOwnProperty(key) ?
+        console.log(key) :  console.log(`Por heanca ${}`)
+ }
