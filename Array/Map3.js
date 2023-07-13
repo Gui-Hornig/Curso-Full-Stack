c8b3eecc2431ceb160133870c9d67d9697174584
Gui-hornig <guihornig@gmail.com>
2023-07-13 14:46:34 -0300

    Update Map3.js

--- Array/Map3.js
+++ Array/Map3.js
@@ -1,4 +1,10 @@
-Array.prototype.map2 = function (callback) {}
+Array.prototype.map2 = function (callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++){
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
 
 const carrinho = [
     '{"nome": "Borracha", "preco": 3.45}',
