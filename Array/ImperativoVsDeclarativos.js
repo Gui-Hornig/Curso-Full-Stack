6e91b081687852568af18b9ee2f5efab7697678b
Gui-hornig <guihornig@gmail.com>
2023-07-13 16:05:52 -0300

    Update ImperativoVsDeclarativos.js

--- Array/ImperativoVsDeclarativos.js
+++ Array/ImperativoVsDeclarativos.js
@@ -11,3 +11,11 @@ for (let i = 0; i < alunos.length; i++) {
 }
 
 console.log(total1 / alunos.length)
+
+//Declartiva
+
+const  getNota = aluno => aluno.nota
+const soma = (total, atual) => total +atual
+
+const total2 = alunos.map(getNota).reduce(soma)
+console.log(total1/ alunos.length)
