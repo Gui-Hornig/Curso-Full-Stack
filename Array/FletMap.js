414f75e7dd407238997cfd0ca892c1db2e0ce5cd
Gui-hornig <guihornig@gmail.com>
2023-07-13 16:21:05 -0300

    Update FletMap.js

--- Array/FletMap.js
+++ Array/FletMap.js
@@ -1,1 +1,19 @@
-const turma = []
+const escola  = [{
+    nome: 'Turma M1',
+    alunos: [{
+        nome: 'Gustavo',
+        nota: 8.1
+    }, {
+        nome: 'Ana',
+        notas: 9.3
+    }]
+}, {
+    nome : 'turma M2',
+    aluno: [{
+        nome: 'Rebeca',
+        nota: 8.9
+    }, {
+        nome: 'Roberto',
+        nota: 7.3
+    }]
+}]
