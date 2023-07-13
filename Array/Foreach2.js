caa307a2309a654ab9a42dc13bc5a3d641b2c7cf
Gui-hornig <guihornig@gmail.com>
2023-07-13 13:21:50 -0300

    Update Foreach2.js

--- Array/Foreach2.js
+++ Array/Foreach2.js
@@ -1,5 +1,6 @@
 Array.prototype.forEach2 = function(callback) {
-    
+    for (let i = 0;  i < this.length; i++)
+    callback (this[i], i, this)
 }
 
 const aprovados = ['agatha', 'Aldo', 'Daniel', 'Raquel' ]
