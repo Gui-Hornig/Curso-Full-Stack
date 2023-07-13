8fc1cbf3fd260c53c2a31e45f94daae238d39463
Gui-hornig <guihornig@gmail.com>
2023-07-13 16:31:18 -0300

    Update FletMap.js

--- Array/FletMap.js
+++ Array/FletMap.js
@@ -26,4 +26,8 @@ console.log(notas1)
 
 console.log([].concat([ 8.1, 9.3 ], [ 8.9, 7.3 ]))
 
-Array.prototype.flatMap
+Array.prototype.flatMap = function(callback){
+    return Array.prototype.concat.apply([], this.map(callback))
+}
+const notas2 = escola.flatMap(getNotasDaTurma)
+console.log(notas2)
