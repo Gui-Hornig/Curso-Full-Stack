966938a115d6abb9606a6e324c80788ab6039f37
Gui-hornig <guihornig@gmail.com>
2023-07-13 15:00:49 -0300

    Update Filter1.js

---
+++ Array/Filter1.js
@@ -0,0 +1,10 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true},
+    { nome: 'Ipad Pro', preco: 4199, fragil: true},
+    { nome: 'Celular Apple', preco: 8000, fragil: true},
+    { nome: 'Copo de plastico', preco: 1.99, frasil: false}
+]
+
+console.log(produtos.filter(function(p){
+    return false
+}))
