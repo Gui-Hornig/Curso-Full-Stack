0602e724a62a662331a9c73c7d96d0cd91fc3e62
Gui-hornig <guihornig@gmail.com>
2023-07-13 14:37:55 -0300

    Update Map2.js

--- Array/Map2.js
+++ Array/Map2.js
@@ -1,6 +1,6 @@
-const carrinho = {
+const carrinho = [
     '{"nome": "Borracha", "preco": 3.45}',
     '{"nome": "Caderno", "preco": 13.90}',
     '{"nome": "Caneta", "preco": 7.50}'
-}
+]
 //Retornar um array apenas com os precos
