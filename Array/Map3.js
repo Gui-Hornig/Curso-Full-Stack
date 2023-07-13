b56952a4a6fbe8e05f33751d88883035bc3d113d
Gui-hornig <guihornig@gmail.com>
2023-07-13 14:41:41 -0300

    Update Map3.js

---
+++ Array/Map3.js
@@ -0,0 +1,12 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+//Retornar um array apenas com os precos
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
