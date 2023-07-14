736960a899f8f2d04c5c66759ccd4645881348f5
Gui-hornig <guihornig@gmail.com>
2023-07-14 14:19:41 -0300

    Update ManimmulandoLinhas.sql

--- BancoDeDadosSQL/ManimmulandoLinhas.sql
+++ BancoDeDadosSQL/ManimmulandoLinhas.sql
@@ -4,3 +4,6 @@ insert into cursos values
 ('1', 'HTML4','Curso de HTML5','40','37','2014'),
 ('2','Algoritimo', 'Logica de Programacao', '20', '15','2014'),
 ('3','Photoshop', 'Dicas de Phooshop CC', '10', '8','2014'),
+('4','PGP', 'Curso de PHP para Iniciantes', '40', '20','2010'),
+('5','Jarva', 'Introducao a linguagem Java', '10', '29','2000'),
+('6','MySQL', 'Banco de Dados MySQL', '30', '15','2016'),
