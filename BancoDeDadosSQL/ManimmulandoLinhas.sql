5b5ef8a94be67d873014a7397c191aa6c367fa83
Gui-hornig <guihornig@gmail.com>
2023-07-14 14:31:05 -0300

    Update ManimmulandoLinhas.sql

--- BancoDeDadosSQL/ManimmulandoLinhas.sql
+++ BancoDeDadosSQL/ManimmulandoLinhas.sql
@@ -17,3 +17,12 @@ insert into cursos values
 update cursos
 set nome = 'HTML5'
 where idcurso ='1';
+
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
+
+update cursos
+set nome ='Java', carga = '40', ano = '2015'
+where idcurso ='5'
+limit 1; /*Limita a uma linha modificada*/
