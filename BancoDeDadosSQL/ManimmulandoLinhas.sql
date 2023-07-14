0d66775f6aaa8989959a264e64699b8dd78c1920
Gui-hornig <guihornig@gmail.com>
2023-07-14 14:19:33 -0300

    Update ManimmulandoLinhas.sql

--- BancoDeDadosSQL/ManimmulandoLinhas.sql
+++ BancoDeDadosSQL/ManimmulandoLinhas.sql
@@ -2,3 +2,5 @@ use cadastro;
 select*from pessoas;
 insert into cursos values
 ('1', 'HTML4','Curso de HTML5','40','37','2014'),
+('2','Algoritimo', 'Logica de Programacao', '20', '15','2014'),
+('3','Photoshop', 'Dicas de Phooshop CC', '10', '8','2014'),
