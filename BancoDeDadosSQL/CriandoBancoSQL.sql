ff2d4d28d10399b67fe3e1b165e34f8276571f56
Gui-hornig <guihornig@gmail.com>
2023-07-14 12:03:45 -0300

    Update CriandoBancoSQL.sql

--- BancoDeDadosSQL/CriandoBancoSQL.sql
+++ BancoDeDadosSQL/CriandoBancoSQL.sql
@@ -1,6 +1,10 @@
+/*Criando banco*/ 
+
 create database cadastroclientes
 default character set utf8
 default collate utf8_general_ci;
+
+/*Adicionando informacoes*/ 
 
 create table pessoas (
 id int not null auto_increment,
