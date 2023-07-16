5aeb2e4b9fb8be60d6507d8921390b7e77bb2fba
Gui-hornig <guihornig@gmail.com>
2023-07-16 19:37:17 -0300

    Update SelectOptendoDados.sql

--- BancoDeDadosSQL/SelectOptendoDados.sql
+++ BancoDeDadosSQL/SelectOptendoDados.sql
@@ -8,3 +8,11 @@ where nome like '%A'; /*Seleciona todos os registros que começam com A */
 where nome like 'PH%P'; /*Seleciona registro que começam com PH e terna com P*/
 where nome like 'PH%P_'; /*Utilizando o '_' vc exige que tenha algum caractere (qualquer)no final*/
 where nome like '%silva%'; /*pesquisa no banco de dados qualquer pessoa que tenha SILVA no nome*/
+
+/*distinct*/
+
+select*from gafanhotos
+where distinct nacionalidade from gafanhotos /*O 'Distinct' agrupa todos os registro selecionados. No ex. oi filtrado os alunos de cada nacidos em cada pais*/
+order by nacionalidade; 
+
+select count(*) from cursos where cargas > 40; /*O operador 'count' conta os registros*/
