cd78b357b447e993d17d99c84dcc24de072b1a01
Gui-hornig <guihornig@gmail.com>
2023-07-16 18:46:22 -0300

    Update SelectOptendoDados.sql

--- BancoDeDadosSQL/SelectOptendoDados.sql
+++ BancoDeDadosSQL/SelectOptendoDados.sql
@@ -1,2 +1,2 @@
-select * from cursos
-where nome like 'P%';
+select * from cursos /*Como o operador 'LIKE' 'P%' voce pode selecionar todos os cursos que comeca com a letra P*/
+where nome like 'P%'; 
