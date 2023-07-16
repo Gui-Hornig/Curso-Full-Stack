3033035a40a13d7d76c012810b61e575ea5d3fb5
Gui-hornig <guihornig@gmail.com>
2023-07-16 19:07:59 -0300

    Update SelectOptendoDados.sql

--- BancoDeDadosSQL/SelectOptendoDados.sql
+++ BancoDeDadosSQL/SelectOptendoDados.sql
@@ -3,7 +3,8 @@ where nome like 'P%';
 
 
 select * from cursos
-where nome like '%A'; /*a posicao do '%' altera o que o SQL retorna*/ 
+where nome like '%A'; /*a posicao do '%' altera o que o SQL retorna*/
 where nome like '%A'; /*Seleciona todos os registros que começam com A */
-where nome like 'PH%P'; /*Seleciona registro que começam com PH e terna com P*/ 
-where nome like 'PH%P_'; /*Utilizando o '_' vc exige que tenha algum caractere no final*/
+where nome like 'PH%P'; /*Seleciona registro que começam com PH e terna com P*/
+where nome like 'PH%P_'; /*Utilizando o '_' vc exige que tenha algum caractere (qualquer)no final*/
+where nome like '%silva%'; /*pesquisa no banco de dados qualquer pessoa que tenha SILVA no nome*/
