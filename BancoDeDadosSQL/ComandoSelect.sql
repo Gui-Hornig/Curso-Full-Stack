5552b94be3b7b083800a39e1632f9cff2900056e
Gui-hornig <guihornig@gmail.com>
2023-07-16 16:36:08 -0300

    Update ComandoSelect.sql

--- BancoDeDadosSQL/ComandoSelect.sql
+++ BancoDeDadosSQL/ComandoSelect.sql
@@ -2,5 +2,9 @@ use cadastro;
 select*from gafanhotos;
 select*from cursos;
 
-select * from cursos 
+select * from cursos  /*Seleciona todas as colunas  */
 order by nome desc; /*Organiza em ordem alfabetica (caso adicione o "DESC" ele ordena em ordem alfabetica de baixo para cima)*/
+
+/*Seleciona apenas as descritas */
+
+sele
