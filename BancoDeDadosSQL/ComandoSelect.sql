7853b38fdd0006fa8d4091e4ee4ebeda50998b30
Gui-hornig <guihornig@gmail.com>
2023-07-16 16:38:06 -0300

    Update ComandoSelect.sql

--- BancoDeDadosSQL/ComandoSelect.sql
+++ BancoDeDadosSQL/ComandoSelect.sql
@@ -6,6 +6,6 @@ select * from cursos  /*Seleciona todas as colunas  */
 order by nome desc; /*Organiza em ordem alfabetica (caso adicione o "DESC" ele ordena em ordem alfabetica de baixo para cima)*/
 
 /*Seleciona apenas as descritas */
-
+/*Caso mude a ordem dos nomes o SQL retorna como vc descreveu*/
 select nome, carga, ano from cursos
 order by nome;
