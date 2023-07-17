2fc7c87bcfe1e67b308da4dd96804015c15db466
Gui-hornig <guihornig@gmail.com>
2023-07-16 23:02:11 -0300

    Update CriacaoModeloRelcional.sql

--- BancoDeDadosSQL/CriacaoModeloRelcional.sql
+++ BancoDeDadosSQL/CriacaoModeloRelcional.sql
@@ -6,4 +6,5 @@ todas sao 'Engine'
 ACID
 A = Atomicidade, nao pode ser dividida em subtarefas ou tudo acontece ou nada e considerado 
 C = Consistencia, se antes de fazer a transacao estava OK, depois da transacao precisa estar OK
-I = 
+I = isolamento, quando tem duas transacaoes, elas precisao ser de modo isolado sem afetar a outra
+D = Durabilidade, precisa ter os dados salvos o tempo necessario nao pode perder dados
