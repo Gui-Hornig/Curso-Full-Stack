abf74ed9fc1ac2b25fc60edcd3f2f0d65d178f60
Gui-hornig <guihornig@gmail.com>
2023-07-16 22:16:15 -0300

    Update ModeloRelacional.sql

--- BancoDeDadosSQL/ModeloRelacional.sql
+++ BancoDeDadosSQL/ModeloRelacional.sql
@@ -1,1 +1,8 @@
 /*os dados tem relacoes entre eles, a partir do cadastro de um "pessoa" voce pode ter acesso aos dados relacionados a pessoa*/
+
+/*Entidade = Curso
+  Atributos = Idcurso
+              nome 
+              descricao 
+              carga 
+              ano*/
