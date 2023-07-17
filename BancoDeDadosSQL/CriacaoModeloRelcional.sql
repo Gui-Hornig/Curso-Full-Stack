6ba2ec5802b2e667353e42c5ddd720d6abac9c89
Gui-hornig <guihornig@gmail.com>
2023-07-16 23:00:26 -0300

    Update CriacaoModeloRelcional.sql

--- BancoDeDadosSQL/CriacaoModeloRelcional.sql
+++ BancoDeDadosSQL/CriacaoModeloRelcional.sql
@@ -2,3 +2,8 @@ InnoDB e uma 'Engine' um mecanismo que suporta chaves estrangeiras
 MyISAM -> nao da suporte as 4 regra principais para uma BOA transacao
 XtraDB  
 todas sao 'Engine' 
+
+ACID
+A = Atomicidade, nao pode ser dividida em subtarefas ou tudo acontece ou nada e considerado 
+C = Consistencia, se antes de fazer a transacao estava OK, depois da transacao precisa estar OK
+I = 
