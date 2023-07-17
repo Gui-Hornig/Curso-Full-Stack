6ac48319e10d90ca41772ef0e49cf27db7f7957d
Gui-hornig <guihornig@gmail.com>
2023-07-17 10:42:51 -0300

    node

---
+++ Node/moduloA.js
@@ -0,0 +1,11 @@
+//Node um ambiente de execusao do JS no seervidor, usa o V8
+
+//runtime JS formado pelo V8 + LIBUV 
+
+// Em node um arquivo  representa um MODULO
+
+//Exportando algo no NODE
+
+this.ola = 'Fala pessoal'
+exports.bemVindo = 'Bem vindo ao NODE!'
+module.exports.ateLogo = 'Ate o proximo exemplo'
