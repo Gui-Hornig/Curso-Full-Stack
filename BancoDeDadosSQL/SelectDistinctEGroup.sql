d55971d51c4e38fa4f5893d3727370429d91a600
Gui-hornig <guihornig@gmail.com>
2023-07-16 21:55:24 -0300

    Update SelectDistinctEGroup.sql

--- BancoDeDadosSQL/SelectDistinctEGroup.sql
+++ BancoDeDadosSQL/SelectDistinctEGroup.sql
@@ -7,3 +7,9 @@
 
 select * from cursos where totaulas > 30;
 select carga, count(nome) from cursos where totaulas = 30 group by carga;
+
+/*Selecionando os anos da tabela de curso, onde (where) o 'totaulas' seja maior que 30, vou agrupar(group by) a selecao por ano
+dentro do agrupamento so vai mostrar quem tem (having) o ano maior (>) que 2013 
+e ordenar (order by ) pelo total*/
+
+selec
