f0a14bbda57392f94e6bec2d15bb7922683af0f8
Gui-hornig <guihornig@gmail.com>
2023-07-17 19:21:17 -0300

    perguntasErespostas

--- PerguntasERespostasSITE/scrypt.js
+++ PerguntasERespostasSITE/scrypt.js
@@ -1,4 +1,18 @@
-function go(){
-    var valueInput = document.getElementById('num').value
-    alert(valueInput)
-}
+function go() {
+    var valueInput = document.getElementById('num').value;
+  
+    if (valueInput === 'guilherme') {
+      alert('acertou');
+    } else {
+      alert('Que pena! Você errou, tente novamente.');
+    }
+  } // se a resposta da pessoa foi certa ele retorna 'acertou' 
+  
+
+
+
+////if (resposta === "exemplo") {
+   // console.log("Resposta válida!");
+////} else {
+    //console.log("Resposta inválida!");
+// }
