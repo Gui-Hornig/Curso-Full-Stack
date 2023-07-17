3f1f90907eaa425dc5287c049b84b7292701ba2c
Gui-hornig <guihornig@gmail.com>
2023-07-16 23:12:39 -0300

    Update CriacaoModeloRelcional.sql

--- BancoDeDadosSQL/CriacaoModeloRelcional.sql
+++ BancoDeDadosSQL/CriacaoModeloRelcional.sql
@@ -1,4 +1,4 @@
-InnoDB e uma 'Engine' um mecanismo que suporta chaves estrangeiras
+/* InnoDB e uma 'Engine' um mecanismo que suporta chaves estrangeiras
 MyISAM -> nao da suporte as 4 regra principais para uma BOA transacao
 XtraDB  
 todas sao 'Engine' 
@@ -7,4 +7,14 @@ ACID
 A = Atomicidade, nao pode ser dividida em subtarefas ou tudo acontece ou nada e considerado 
 C = Consistencia, se antes de fazer a transacao estava OK, depois da transacao precisa estar OK
 I = isolamento, quando tem duas transacaoes, elas precisao ser de modo isolado sem afetar a outra
-D = Durabilidade, precisa ter os dados salvos o tempo necessario nao pode perder dados
+D = Durabilidade, precisa ter os dados salvos o tempo necessario nao pode perder dados  */
+
+use cadastro;
+desc gafanhotos;
+
+alter table gafanhotos
+add column cursopreferido int;
+/*O cursopreferido da tabela gafanhotos esta ligados com idcursos da tabela cursos */
+alter table gafanhoto
+add foreign key(cursopreferido)
+references cursos(idcursos);
