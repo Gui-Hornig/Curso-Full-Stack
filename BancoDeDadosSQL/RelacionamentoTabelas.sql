99a337a27c2eff14623b46ba72d723b63d3db9ee
Gui-hornig <guihornig@gmail.com>
2023-07-17 00:05:34 -0300

    Create RelacionamentoTabelas.sql

---
+++ BancoDeDadosSQL/RelacionamentoTabelas.sql
@@ -0,0 +1,13 @@
+/* Relacionamento muitos pra Muitos
+A chave estrangeira precisa ser do mesmo tipo da chave primaria */
+
+create table gafanhoto_assite_curso (
+    id int NOT NULL AUTO_INCREMENT,
+    data date,
+    idgafanhoto int,
+    idcurso int,
+    PRIMARY KEY(id),
+    FOREIGN KEY(idgafanhoto) REFERENCES gafanhotos(id),
+    FOREIGN KEY(Idcurso) REFERENCES cursos(idcurso)
+)DEFAULT CHARSET = utf8;
+
