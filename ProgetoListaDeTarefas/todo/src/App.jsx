0fc2a78b1212290f370d6e5d644f14f6553513aa
Gui-hornig <guihornig@gmail.com>
2023-07-18 11:14:04 -0300

    perguntasErespostas

--- ProgetoListaDeTarefas/todo/src/App.jsx
+++ ProgetoListaDeTarefas/todo/src/App.jsx
@@ -29,6 +29,11 @@ import { useState } from 'react'
           <div className="todo">
             <div className="content">
               <p>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>X</button>
             </div>
           </div>
         ))}
