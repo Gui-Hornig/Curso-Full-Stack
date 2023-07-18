9f0ea4948d67e6fdcbe1abecb8b02313ee826e76
Gui-hornig <guihornig@gmail.com>
2023-07-18 13:00:28 -0300

    perguntasErespostas

--- ProgetoListaDeTarefas/todo/src/App.jsx
+++ ProgetoListaDeTarefas/todo/src/App.jsx
@@ -1,42 +1,59 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Todo from './components/Todo'
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 
 import "./App.css";
-import TodoForm from './components/TodoForm';
 
-  function App() {
-    const [todos, settodos] = useState([
-      {
-        id: 1,
-        text: "Criar funcionalidades X no sistema",
-        category: "Trabalho",
-        isCompleted: false,
-      },
-      {
-        id: 2,
-        text: "Ir para a academia",
-        category: "Pessoal",
-        isCompleted: false,
-      },
-      {
-        id: 3,
-        text: "Estudar React",
-        category: "Estudos",
-        isCompleted: false,
-      },
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidades X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
 
-    ]);
-    return <div className="app">
-      <h1>Lista de tarefas</h1>
-      <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo todo={todo}/>
-        ))}
-      </div>
-          <TodoForm />
+  ]);
+
+  const addTodo = (text, category) => {
+
+     const newTodos = [
+       ...todos, 
+       {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+       },
+      ]
+
+      setTodos(newTodos)
+     }
+
+
+  return <div className="app">
+    <h1>Lista de tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
     </div>
-  }
+    <TodoForm addTodo={addTodo} />
+  </div>
+}
 
 
 export default App;
--- ProgetoListaDeTarefas/todo/src/components/TodoForm.jsx
+++ ProgetoListaDeTarefas/todo/src/components/TodoForm.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import {useState} from 'react'
 
-function TodoForm() {
+const TodoForm =({addTodo}) => {
+    const [value, setValue] = useState("")
+    const [category, setCategory] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(!value || !category) return
+        addTodo(value, category)
+        setValue("")
+        setCategory("")
+    }
+
   return (
-    <div>TodoForm</div>
+    <div className="todo-form">
+        <h2>Criar tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Digitar o titulo' value={value} onChange={e => setValue(e.target.value)}/>
+            <select value={category} onChange={e => setCategory(e.target.value)}>
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type='submit'>Criar tarefa</button>
+        </form>
+    </div>
   )
 }
 
