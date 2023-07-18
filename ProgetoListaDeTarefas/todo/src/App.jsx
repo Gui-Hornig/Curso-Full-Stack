2513e5a91b77e3af8d89b26577ba6bd6cc575fe7
Gui-hornig <guihornig@gmail.com>
2023-07-18 10:52:03 -0300

    perguntasErespostas

--- ProgetoListaDeTarefas/todo/src/App.jsx
+++ ProgetoListaDeTarefas/todo/src/App.jsx
@@ -5,6 +5,29 @@ function App() {
 
   return <div>  Ola react </div>
   
+  function App() {
+    const [todos, settodos] = useState([
+      {
+        id: 1,
+        text: "Criar funcionalidades X no sistema",
+        category: "Trabalho",
+        isCompleted: false,
+      },
+      {
+        id: 2,
+        text: "Ir para a academia",
+        category: "Pessoal",
+        isCompleted: false,
+      },
+      {
+        id: 3,
+        text: "Estudar React",
+        category: "Estudos",
+        isCompleted: false,
+      },
+
+    ])
+  }
 }
 
 export default App
