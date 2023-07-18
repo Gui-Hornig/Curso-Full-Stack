70161cc41a8c914d33044e9aaa42681bc6bb1755
Gui-hornig <guihornig@gmail.com>
2023-07-18 11:50:16 -0300

    perguntasErespostas

--- ProgetoListaDeTarefas/todo/src/App.jsx
+++ ProgetoListaDeTarefas/todo/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
+
+import Todo from './components/Todo'
+
 import "./App.css";
+import TodoForm from './components/TodoForm';
 
   function App() {
     const [todos, settodos] = useState([
@@ -27,19 +31,10 @@ import "./App.css";
       <h1>Lista de tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">({todo.category})</p>
-            </div>
-            <div>
-              <button>Completar</button>
-              <button>X</button>
-            </div>
-          </div>
+          <Todo todo={todo}/>
         ))}
       </div>
-
+          <TodoForm />
     </div>
   }
 
