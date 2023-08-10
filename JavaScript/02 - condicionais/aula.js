49f2ccf14552c2d41b12366bc073a72ffdbfc973
Gui-hornig <guihornig@gmail.com>
2023-08-10 16:23:56 -0300

    Update aula.js

--- JavaScript/02 - condicionais/aula.js
+++ JavaScript/02 - condicionais/aula.js
@@ -5,3 +5,15 @@ const numero = 10;
 const numeroPar = (numero % 2) === 0;
 
 console.log(numeroPar);
+
+
+// If else
+
+
+const idade = 11;
+const verdadeiroEfalso = (idade % 2) === 0;
+console.log(verdadeiroEfalso)
+
+if (verdadeiroEfalso) {
+    console.log('Executei')
+}
