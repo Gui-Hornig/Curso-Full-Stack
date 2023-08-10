98daabfc79ea6f7eaff5abe6d26e79a9409d9238
Gui-hornig <guihornig@gmail.com>
2023-08-10 16:52:34 -0300

    condicionais

---
+++ JavaScript/02 - condicionais/desafio.js
@@ -0,0 +1,11 @@
+// Faça um programa para calcular o valor de uma viagem
+
+// Você terá 5 variáveis, sendo elas:
+
+// 1 - Preço do combustivel 
+// 2 - Preço da gasolina
+// 3 - O tipo de combustivel que está no carro
+// 4- gasto médio de combustivel do carro por KM 
+// 5 - distancia em KM da viagem
+
+// Imprima no Console o gasto para realizar a viagem
