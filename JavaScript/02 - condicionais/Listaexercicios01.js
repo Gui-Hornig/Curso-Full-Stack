f9be3a589556df862ff30bf46e52a02894cc8235
Gui-hornig <guihornig@gmail.com>
2023-08-11 15:43:48 -0300

    Update Listaexercicios01.js

--- JavaScript/02 - condicionais/Listaexercicios01.js
+++ JavaScript/02 - condicionais/Listaexercicios01.js
@@ -1,3 +1,9 @@
 // faça um algoritimo que dado as 3 notas tiradas por um aluno em um semestre da faculdade e imprima 
 // sua média e sua classificação conforme a tabela abaixo. 
 
+// Méddia = (nota 1 + nota 2 + nota 3 ) / 3
+
+// Classificação:
+//  - Média menor que 5, recuperação
+//  - Média entre 5 e 7, recuperação
+//  - Média acima de 7, passou de semestre
