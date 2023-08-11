8d1b53a43def89a1071ff6232fc1eeda79a0eebe
Gui-hornig <guihornig@gmail.com>
2023-08-11 15:41:45 -0300

    Update Listaexercicios01.js

--- JavaScript/02 - condicionais/Listaexercicios01.js
+++ JavaScript/02 - condicionais/Listaexercicios01.js
@@ -1,1 +1,3 @@
-// faça um algoritimo que dado as 3 notas tiradas 
+// faça um algoritimo que dado as 3 notas tiradas por um aluno em um semestre da faculdade e imprima 
+// sua média e sua classificação conforme a tabela abaixo. 
+
