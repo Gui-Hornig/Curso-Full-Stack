140fbd325edec1aa5b0e2ce41317681de4ae614d
Gui-hornig <guihornig@gmail.com>
2023-08-13 14:42:16 -0300

    Update Listaexercicios02.js

---
+++ JavaScript/02 - condicionais/Listaexercicios02.js
@@ -0,0 +1,19 @@
+// 2 - O IMC - indice de massa corporal é um criterio de organização mundial de saude para dar uma indicação
+// sobre a condição de peso de uma pessoa adulta.
+
+// Formula do IMC : 
+
+// IMC = peso / (altura * altura)
+
+// Elabore um algoritimo que dado o peso e a altura de um adulto mostre sua condição de acordo com a tabela abaixo. 
+
+// IMC em adultos condições: 
+
+// - Abaixo de 18.5 = Abaixo do peso;
+// - Entre 18.5 e 25 = Peso normal;
+// - Entre 25 e 30 = Acima do peso;
+// - Entre 30 e 40 = Obeso ;
+// - Acima de 40 = Obesidade Grave;
+
+
+conms
