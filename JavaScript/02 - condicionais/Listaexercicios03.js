c9e16bc638db11c5e517a5879e75a11533b0fc06
Gui-hornig <guihornig@gmail.com>
2023-08-13 19:01:37 -0300

    Update Listaexercicios03.js

---
+++ JavaScript/02 - condicionais/Listaexercicios03.js
@@ -0,0 +1,15 @@
+// 3 - Elabore um algoritimo que calcule o que deve ser pago por um produto, considerando o preço normal da etiqueta
+// e a escolha da condição de pagamento.
+// Utilize os códigos da tabela a seguir paa ler qual a condição de pagamento escolhido e efetuar o cálculo adequado.
+
+// Código condição de pagamento: 
+
+// - A vista débito, recebe 10% de desconto;
+// - A vista no dinheiro ou PIX, recebe 15% de desconto;
+// - Em duas vezes, preço normal da etiqueta sem juros;
+// - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+
+const valorProduto = 100.00
+const debito = -10%
+
+if (de){}
