e669e60b5a15430c81f6cfbd6b30bf2fd58b0761
Gui-hornig <guihornig@gmail.com>
2023-08-13 01:54:25 -0300

    Update Listaexercicios01.js

--- JavaScript/02 - condicionais/Listaexercicios01.js
+++ JavaScript/02 - condicionais/Listaexercicios01.js
@@ -16,7 +16,7 @@ const mediaAluno = (notaA + notaB + notaC) / 3
 
 if (mediaAluno < 5) {
     console.log('Recuperacao')
-} else if (mediaAluno = 5  7) {
+} else if (mediaAluno = 5 || 7) {
     console.log('Recuperacao')
 }
 else (mediaAluno > 7) {
