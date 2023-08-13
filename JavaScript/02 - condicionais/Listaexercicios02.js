93bcf482f4d2893a67cd25e4f3f80bc81ff5aab8
Gui-hornig <guihornig@gmail.com>
2023-08-13 15:10:07 -0300

    Update Listaexercicios02.js

--- JavaScript/02 - condicionais/Listaexercicios02.js
+++ JavaScript/02 - condicionais/Listaexercicios02.js
@@ -29,4 +29,4 @@ if (calculoImc < 18.5) {
     console.log('Cuidado! Você está acima do peso, seu IMC atutal é: ', calculoImc.toFixed(2))
 } else if (calculoImc >= 30 && calculoImc <= 40){
     console.log('Para de comer AGORA! Você está obeso, seu IMC atual é: ', calculoImc.toFixed(2))
-} else {console.log ('Você está correndo sérios riscos de saúde, seu IMC atual é: ', calculoImc.toFixed(2))}
+} else {console.log ('OBESIDADE GRAVE! Você está correndo sérios riscos de saúde, seu IMC atual é: ', calculoImc.toFixed(2))}
