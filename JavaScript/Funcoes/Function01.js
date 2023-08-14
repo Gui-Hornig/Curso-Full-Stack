f6fdbb051ed677c173bb2dded1023eaaf5fbfe45
Gui-hornig <guihornig@gmail.com>
2023-08-14 13:37:21 -0300

    Create Function01.js

---
+++ JavaScript/Funcoes/Function01.js
@@ -0,0 +1,7 @@
+// Um pequeno trecho de código que podemos invocar novamente para utilizar novamente.
+
+function seyMyName(name) {
+    console.log('Your name is: ' + name)
+}
+
+seyMyName('Guilherme')
