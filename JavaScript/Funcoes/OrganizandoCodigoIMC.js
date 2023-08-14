fd0695a03caa14116c7fb7f0acc97c796a4bb352
Gui-hornig <guihornig@gmail.com>
2023-08-14 13:58:17 -0300

    Update OrganizandoCodigoIMC.js

--- JavaScript/Funcoes/OrganizandoCodigoIMC.js
+++ JavaScript/Funcoes/OrganizandoCodigoIMC.js
@@ -4,12 +4,23 @@ const altura = 1.75;
 // Math.pow(altura, 2) Utilizando esse biblioteca podemos fazer numeros ao quadradro, cubo etc...
 const calculoImc = peso / (altura * altura);
 
-if (calculoImc < 18.5) {
-    console.log ('Você está abaixo do peso, seu IMC atual é: ',calculoImc.toFixed(2));
-} else if (calculoImc >= 18.5 && calculoImc < 25){
-    console.log ('Seu peso está normal, continue cuidando da saúde. IMC atutal é: ', calculoImc.toFixed(2));
-} else if (calculoImc >= 25 && calculoImc < 30 ){
-    console.log('Cuidado! Você está acima do peso, seu IMC atutal é: ', calculoImc.toFixed(2));
-} else if (calculoImc >= 30 && calculoImc <= 40){
-    console.log('Para de comer AGORA! Você está obeso, seu IMC atual é: ', calculoImc.toFixed(2));
-} else {console.log ('OBESIDADE GRAVE! Você está correndo sérios riscos de saúde, seu IMC atual é: ', calculoImc.toFixed(2))};
+
+
+// REFATORANDO O CODIGO PARA UTILIZAR FUNCOES
+
+function calcularImc(peso, altura) {
+    return peso / Math.pow(altura, 2);
+}
+
+function classificarImc (calculoImc) {
+    if (calculoImc < 18.5) {
+        console.log ('Você está abaixo do peso, seu IMC atual é: ',calculoImc.toFixed(2));
+    } else if (calculoImc >= 18.5 && calculoImc < 25){
+        console.log ('Seu peso está normal, continue cuidando da saúde. IMC atutal é: ', calculoImc.toFixed(2));
+    } else if (calculoImc >= 25 && calculoImc < 30 ){
+        console.log('Cuidado! Você está acima do peso, seu IMC atutal é: ', calculoImc.toFixed(2));
+    } else if (calculoImc >= 30 && calculoImc <= 40){
+        console.log('Para de comer AGORA! Você está obeso, seu IMC atual é: ', calculoImc.toFixed(2));
+    } else {console.log ('OBESIDADE GRAVE! Você está correndo sérios riscos de saúde, seu IMC atual é: ', calculoImc.toFixed(2))};
+    
+}
