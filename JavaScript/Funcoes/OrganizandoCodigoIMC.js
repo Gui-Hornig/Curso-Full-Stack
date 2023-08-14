036be30e33a8bd651672da2a571008702312ef39
Gui-hornig <guihornig@gmail.com>
2023-08-14 14:00:45 -0300

    Update OrganizandoCodigoIMC.js

--- JavaScript/Funcoes/OrganizandoCodigoIMC.js
+++ JavaScript/Funcoes/OrganizandoCodigoIMC.js
@@ -14,13 +14,15 @@ function calcularImc(peso, altura) {
 
 function classificarImc (calculoImc) {
     if (calculoImc < 18.5) {
-        console.log ('Você está abaixo do peso, seu IMC atual é: ',calculoImc.toFixed(2));
+        return  'Você está abaixo do peso, seu IMC atual é: ',calculoImc.toFixed(2);
     } else if (calculoImc >= 18.5 && calculoImc < 25){
-        console.log ('Seu peso está normal, continue cuidando da saúde. IMC atutal é: ', calculoImc.toFixed(2));
+        return  'Seu peso está normal, continue cuidando da saúde. IMC atutal é: ', calculoImc.toFixed(2);
     } else if (calculoImc >= 25 && calculoImc < 30 ){
-        console.log('Cuidado! Você está acima do peso, seu IMC atutal é: ', calculoImc.toFixed(2));
+        return 'Cuidado! Você está acima do peso, seu IMC atutal é: ', calculoImc.toFixed(2);
     } else if (calculoImc >= 30 && calculoImc <= 40){
-        console.log('Para de comer AGORA! Você está obeso, seu IMC atual é: ', calculoImc.toFixed(2));
-    } else {console.log ('OBESIDADE GRAVE! Você está correndo sérios riscos de saúde, seu IMC atual é: ', calculoImc.toFixed(2))};
+        return 'Para de comer AGORA! Você está obeso, seu IMC atual é: ', calculoImc.toFixed(2);
+    } else 
+        {return 'OBESIDADE GRAVE! Você está correndo sérios riscos de saúde, seu IMC atual é: ', calculoImc.toFixed(2)
+    };
     
 }
