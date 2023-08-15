37d7ff1b8a2a9423cdc6375c38ad34be14717fe3
Gui-hornig <guihornig@gmail.com>
2023-08-15 15:22:50 -0300

    Update ExercicioFuncao01.js

--- JavaScript/Funcoes/ExercicioFuncao01.js
+++ JavaScript/Funcoes/ExercicioFuncao01.js
@@ -2,3 +2,13 @@ function escreveSeuNome(nome, cargo) {
     console.log('Olá! Meu nome é '+ nome +' Sou '+ cargo +' a 3 anos.')
 }
 escreveSeuNome('Guilherme.', 'engenheiro de Software')
+
+
+// Funcao que fverifica se o usuario e maior de 18 ano
+
+
+function verificarIdade (maiorDeIdade) {
+    if (maiorDeIdade >= 18){
+        console.log ('Parabens')
+    }
+}
