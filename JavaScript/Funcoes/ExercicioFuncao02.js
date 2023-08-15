8f6a32177bd1b49d311378778890ab319a52ff9e
Gui-hornig <guihornig@gmail.com>
2023-08-15 16:28:00 -0300

    Update ExercicioFuncao02.js

--- JavaScript/Funcoes/ExercicioFuncao02.js
+++ JavaScript/Funcoes/ExercicioFuncao02.js
@@ -23,12 +23,12 @@ if (formaDePagamento === debito){
 
 function meioDePagamento(formaDePagamento){
     if (formaDePagamento === debito){
-        console.log('Valor total da compra', debito.toFixed(2));
+        return ('Valor total da compra', debito.toFixed(2));
     } else if (formaDePagamento === dinheiroPix){
-        console.log('Valor total da compra', dinheiroPix.toFixed(2));  
+        return ('Valor total da compra', dinheiroPix.toFixed(2));  
     } else if (formaDePagamento === emDuasVezes) {
-        console.log('Duas vezes sem juros o valor total da compra R$',emDuasVezes.toFixed(2));
+        return ('Duas vezes sem juros o valor total da compra R$',emDuasVezes.toFixed(2));
     } else if (formaDePagamento === acimaDe3vezes){
-        console.log('Três vezes com juros de 10%, valor total da compra R$',acimaDe3vezes.toFixed(2));
+        return ('Três vezes com juros de 10%, valor total da compra R$',acimaDe3vezes.toFixed(2));
     }
 }
