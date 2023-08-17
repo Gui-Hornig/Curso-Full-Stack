60899f4fe862d5ed2654ee401affa2eaca061534
Gui-hornig <guihornig@gmail.com>
2023-08-17 16:51:21 -0300

    Update ClasseEinstancias.js

--- JavaScript/ClasseEobjetos/ClasseEinstancias.js
+++ JavaScript/ClasseEobjetos/ClasseEinstancias.js
@@ -1,10 +1,12 @@
-const Humanos = {
-    nome,
-    idade,
-    profissao,
-    salario,
+class Humanos {
+    nome;
+    idade;
+    profissao;
+    salario;
 
     descrever() {
-        console.log(`Sou ${this.nome} tennho ${this.idade}`)
+        console.log(`Sou ${this.nome} tennho ${this.idade}, minha profissao e 
+        ${this.profissao} e ganho ${this.salario} por mes.`);
     }
 }
+
