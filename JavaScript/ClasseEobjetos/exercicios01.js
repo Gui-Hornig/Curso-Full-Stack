f026797e483f2e10a667898a97617513231de754
Gui-hornig <guihornig@gmail.com>
2023-08-18 19:32:22 -0300

    Create exercicios01.js

---
+++ JavaScript/ClasseEobjetos/exercicios01.js
@@ -0,0 +1,7 @@
+/*
+1- Crie uma classe para reprentar carros.
+Os carros possuem uma marca, uma cor  um gasto medio de combustivel por km rodado.
+Crie um metodo que dado a quantidade e opreco do combustivel nos de o valor gasto em reais para realizar este percurso.
+*/
+
+class (){}
