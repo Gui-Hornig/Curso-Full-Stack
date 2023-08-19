d271715124881343715d58e1bd0aef18476509f4
Gui-hornig <guihornig@gmail.com>
2023-08-18 23:02:37 -0300

    Update exercicios2.js

--- JavaScript/ClasseEobjetos/exercicios2.js
+++ JavaScript/ClasseEobjetos/exercicios2.js
@@ -11,13 +11,25 @@ class MedirImc {
     peso;
     altura;
 
-    constructor (nome, peso, altura){
+    constructor (nome, peso, altura, calcularImc){
         this.nome = nome;
         this.peso = peso;
         this.altura = altura;
+        this.calcularImc = calcularImc = peso / (altura * altura) 
     }
 
-    calcularImc (peso, altura) {
-        return peso / (altura * altura)
+    //  calcularImc () {
+    //    return this.peso / (this.altura * this.altura)
+    //}
+    descrever(){
+        console.log(`${this.nome} pesa ${this.peso} e seu IMC e ${this.calcularImc.toFixed(2)}`)
     }
 }
+const pessoa1 = new MedirImc('Jose', 70, 1.70)
+console.log(pessoa1)
+/// let imc = console.log(pessoa1.calcularImc().toFixed(2))
+console.log(pessoa1.descrever());
+
+
+
+
