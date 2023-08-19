2ad1d85e9047749e1d04b66fd5ae032342df415a
Gui-hornig <guihornig@gmail.com>
2023-08-18 21:41:06 -0300

    Create exercicios2.js

---
+++ JavaScript/ClasseEobjetos/exercicios2.js
@@ -0,0 +1,15 @@
+/*
+2 - Crie uma classe para representar pessoas.
+    Para cada pessoa teremos os atributos NOME, PESO, ALTURA.
+    As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+    Instancie uma pessoa chamada JOSÉ que tenha 70kg de peso e 1.75 de altura.
+    Peça ao José para dizer o valor do seu IMC;
+*/ 
+
+class MedirImc {
+    nome;
+    peso;
+    altura;
+
+    
+}
