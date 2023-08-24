f6939ad9af2d2825d11113923e11f697a7ab8009
Gui-hornig <guihornig@gmail.com>
2023-08-24 18:29:41 -0300

    Update index.js

--- Clone/index.js
+++ Clone/index.js
@@ -1,1 +1,6 @@
 <html lang="pt-br"></html>
+
+<head>
+    <meta charset="UTF-8">
+<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
