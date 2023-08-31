578b737a56c40f5b61164e4366ac85ee22bd523f
Gui-hornig <guihornig@gmail.com>
2023-08-31 17:27:38 -0300

    Update funçoes2.js

--- JavaScript/Funcoes/funçoes2.js
+++ JavaScript/Funcoes/funçoes2.js
@@ -3,3 +3,4 @@
 <script type="application/json" data-content-len="101" data-sjs>{"require":[["
 <script type="application/json" data-content-len="99" data-sjs>{"require":[["qplTimingsServerJS",null,null,["7273584970009519893-server","genPreloadersEnd",67]]]}</script><script type="application/json" data-content-len="97" data-sjs>{"require":[["qplTimingsServerJS",null,null,["7273584970009519893-server","preloaderStart",67]]]}</script>
 <script type="application/json" data-content-len="106" data-sjs>{"require":[["qplTimingsServerJS",null,null,["7273584970009519893-server","preloaderThreadsStarted",80]]]}</script>
+<script type="application/json" data-content-len="93" data-sjs>{"require":[["qplTimingsServerJS",null,null,["7273584970009519893-server","earlyFlush",90]]]}</script>
