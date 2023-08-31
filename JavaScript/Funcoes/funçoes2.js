63e47b11b05546f0af8bd96b81a7109d9ca1ba41
Gui-hornig <guihornig@gmail.com>
2023-08-31 17:28:57 -0300

    Update funçoes2.js

--- JavaScript/Funcoes/funçoes2.js
+++ JavaScript/Funcoes/funçoes2.js
@@ -10,3 +10,4 @@
 <script type="application/json" data-content-len="112" data-sjs>{"require":[["qplTimingsServerJS",null,null,["7273584970009519893-server","genTierTwoHasteResponseStart",326]]]}</script>
 <script type="application/json" data-content-len="104" data-sjs>{"require":[["qplTimingsServerJS",null,null,["7273584970009519893-server","ssr_signal_left_rail",361]]]}</script>
 <script type="application/json" data-content-len="102" data-sjs>{"require":[["qplTimingsServerJS",null,null,["7273584970009519893-server","ssr_signal_top_nav",364]]]}</script>
+<script type="application/json" data-content-len="110" data-sjs>{"require":[["qplTimingsServerJS",null,null,["7273584970009519893-server","genTierTwoHasteResponseEnd",405]]]}</script>
