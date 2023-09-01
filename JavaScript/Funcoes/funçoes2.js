be3dca7d987123b0b052b33baa71257b1a952489
Gui-hornig <guihornig@gmail.com>
2023-09-01 19:54:36 -0300

    Update funçoes2.js

--- JavaScript/Funcoes/funçoes2.js
+++ JavaScript/Funcoes/funçoes2.js
@@ -6,4 +6,4 @@ i['GoogleAnalyticsObject'] = r;
         (i[r].q = i[r].q ||  []).push(arguments);
     }),
     (i[r].l = 1 * new Date());
-    (a = s.createElement(o))
+    (a = s.createElement(o))), (m = s.getElementsByTagName(o)[0]);
