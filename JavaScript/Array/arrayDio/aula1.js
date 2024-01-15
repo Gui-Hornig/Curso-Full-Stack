3cd9ca50a579709e7788e75095e5c430cd7ff02e
Gui-hornig <guihornig@gmail.com>
2024-01-15 16:27:05 -0300

    study

---
+++ JavaScript/Array/arrayDio/aula1.js
@@ -0,0 +1,29 @@
+
+
+const alunos = ['Joao', 'Vitor', 'gui', 'natalia']
+
+alunos.push ('rennan')// Adiciona algo a lista
+
+alunos[5] = 'Carlos'
+console.log (alunos)
+
+const calc = ['1', '2', '3']
+console.log(calc)
+console.log(calc.pop()) // Remove o ultimo da da posicao
+console.log(calc)
+
+console.log(calc.shift()) // Remove o Primeiro da posicao
+
+console.log(calc)
+
+
+
+const notas = []
+
+notas.push(5)
+notas.push(7)
+notas.push(8)
+notas.push(2)
+notas.push(5)
+
+console.log(notas.length) // Mostra o tamanho da linta
