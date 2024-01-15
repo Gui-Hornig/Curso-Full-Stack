117a1ea2c5ef47adbc3c62817b0f6b66bbb5c64c
Gui-hornig <guihornig@gmail.com>
2024-01-15 17:03:06 -0300

    Update aula1.js

--- JavaScript/Array/arrayDio/aula1.js
+++ JavaScript/Array/arrayDio/aula1.js
@@ -18,6 +18,23 @@ console.log(calc)
 
 
 
+
+
+const nome = 'Guilherme veroneze Mendes';
+
+for(let i = 0; i < nome.length; i++){
+    console.log(nome[i])
+}
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra)
+    
+}
+
+
+
+
 const notas = []
 
 notas.push(5)
@@ -25,5 +42,15 @@ notas.push(7)
 notas.push(8)
 notas.push(2)
 notas.push(5)
+notas.push(8)
+notas.push(8)
 
-console.log(notas.length) // Mostra o tamanho da linta
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+
+}
+const media = soma / notas.length;
+console.log(media);
