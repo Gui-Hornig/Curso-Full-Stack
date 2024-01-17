cf224300593b8ad534cf202e13641d5012044c12
Gui-hornig <guihornig@gmail.com>
2024-01-17 16:29:39 -0300

    Create excercicio2_array.js

---
+++ JavaScript/Array/arrayDio/excercicio2_array.js
@@ -0,0 +1,11 @@
+// Crie um programa que seja capaz de percorrer uma lista de numeros e imprima cada nuimero par encontrado
+
+const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    console.log(numero)
+    if (numero %2 === 0) {
+        
+    }
+}
