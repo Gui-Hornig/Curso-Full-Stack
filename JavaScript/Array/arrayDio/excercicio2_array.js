cd0531880bc4d51ec2507e4798a109b85ba1973c
Gui-hornig <guihornig@gmail.com>
2024-01-17 16:45:21 -0300

    Update excercicio2_array.js

--- JavaScript/Array/arrayDio/excercicio2_array.js
+++ JavaScript/Array/arrayDio/excercicio2_array.js
@@ -4,8 +4,14 @@ const numeros = [0,1,2,3,4,5,6,7,8,9,10];
 
 for (let i = 0; i < numeros.length; i++) {
     const numero = numeros[i];
-    console.log(numero)
+
     if (numero %2 === 0) {
-        
+        console.log(numero)
     }
 }
+
+
+
+const listaDeValores = [1, 2, 3, 4, 5];
+const valor = listaDeValores[2];
+console.log(valor);
