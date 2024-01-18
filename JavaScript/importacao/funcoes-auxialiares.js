2c01a41318e526f7c096d9781839967ffd2efc0e
Gui-hornig <guihornig@gmail.com>
2024-01-18 16:29:35 -0300

    Update

---
+++ JavaScript/importacao/funcoes-auxialiares.js
@@ -0,0 +1,9 @@
+function gets (){
+    return 10;
+}
+
+function prints (texto) {
+    console.log(texto);
+}
+
+module.exports = {gets, prints};
---
+++ JavaScript/importacao/main.js
@@ -0,0 +1,17 @@
+const funcoes = require('./funcoes-auxialiares');
+
+console.log(funcoes)
+
+console.log(funcoes.gets())
+
+
+const pessoa = {
+    nome: 'vitor'
+};
+
+// Destruir o Objeto
+
+const  {nome} = pessoa;
+
+const {gets, prints} = require('./funcoes-auxialiares')
+prints(gets());
