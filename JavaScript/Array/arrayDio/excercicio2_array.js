ff91b8e8f5a8f25700a8aba53ab326725665fbc1
Gui-hornig <guihornig@gmail.com>
2024-01-22 15:50:34 -0300

    Update excercicio2_array.js

--- JavaScript/Array/arrayDio/excercicio2_array.js
+++ JavaScript/Array/arrayDio/excercicio2_array.js
@@ -15,3 +15,10 @@ for (let i = 0; i < numeros.length; i++) {
 const listaDeValores = [1, 2, 3, 4, 5];
 const valor = listaDeValores[2];
 console.log(valor);
+
+
+const sum = 0 
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
