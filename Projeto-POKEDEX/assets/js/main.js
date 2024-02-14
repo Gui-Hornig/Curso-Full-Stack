2d6a9afd9393006c78ce6dc36ab6b75168bb9254
Gui-hornig <guihornig@gmail.com>
2024-02-14 19:58:58 -0300

    Update main.js

--- Projeto-POKEDEX/assets/js/main.js
+++ Projeto-POKEDEX/assets/js/main.js
@@ -5,5 +5,3 @@ const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 fetch(url).then(function (response) {
   console.log(response)
 })
-
-.catch.apply.arguments9(CSSMatrixComponent)
